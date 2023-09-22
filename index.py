a3a2f2d87098ae4c4e1b99c911d7049178b1ef28
Caique Magalhaes <130615707+Eike94@users.noreply.github.com>
2023-09-22 14:34:19 -0300

    Corrigido
    Faltou algumas linhas que faltou

--- index.py
+++ index.py
@@ -5,24 +5,27 @@ def subtrair_valor_arquivo(nome_arquivo_entrada, nome_arquivo_saida, valor_subtr
     with open(nome_arquivo_entrada, 'r') as arquivo_entrada, open(nome_arquivo_saida, 'w') as arquivo_saida:
         for linha in arquivo_entrada:
             # Verifica se a linha corresponde a um dos padrões
-            padroes = ["CT-", "CF-", "CF\P{\C2;PSC} \P", "CF\P", "{\C5;CF\P"]
+            padroes = ["CT-", "CF-", "CF\P{\C2;PSC} \P", "CF\P", "{\C5;CF\P", "{\C5;CF \P", "CF \P", "CF"]
             if any(padrao in linha for padrao in padroes):
-                # Divide a linha em partes usando espaços como separadores
                 partes = linha.split()
-                for i in range(len(partes)):
-                    # Tenta encontrar números nas partes
-                    numeros = re.findall(r'\d+\,\d+', partes[i])
-                    for numero in numeros:
-                        # Tenta converter o número e subtrair o valor
-                        try:
+                nova_linha = ""
+                for parte in partes:
+                    if re.match(r'(.*\d+\.\d+.*)', parte):
+                        # A parte contém números decimais com "." como separador decimal
+                        numeros = re.findall(r'(\d+\.\d+)', parte)
+                        for numero in numeros:
+                            valor_numerico = float(numero)
+                            novo_valor = valor_numerico - valor_subtrair
+                            parte = parte.replace(numero, "{:.3f}".format(novo_valor).replace('.', ','))
+                    elif re.match(r'(.*\d+,\d+.*)', parte):
+                        # A parte contém números decimais com "," como separador decimal
+                        numeros = re.findall(r'(\d+,\d+)', parte)
+                        for numero in numeros:
                             valor_numerico = float(numero.replace(',', '.'))
                             novo_valor = valor_numerico - valor_subtrair
-                            partes[i] = partes[i].replace(numero, "{:.2f}".format(novo_valor).replace('.', ','))
-                        except ValueError:
-                            pass  # Ignora partes que não podem ser convertidas em número
-                # Recria a linha com as partes atualizadas
-                linha = ' '.join(partes) + '\n'
-            # Escreve a linha no arquivo de saída
+                            parte = parte.replace(numero, "{:.3f}".format(novo_valor).replace('.', ','))
+                    nova_linha += parte + " "
+                linha = nova_linha.strip() + '\n'
             arquivo_saida.write(linha)
 
 # Nome do arquivo de entrada e saída
